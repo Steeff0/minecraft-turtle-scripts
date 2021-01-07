40da28eef336746a5865afe7de1afda6c6c6c755
Steven Gerritsen <17140605+Steeff0@users.noreply.github.com>
2021-01-07 07:23:33 +0100

    Update buildFloor.lua

--- buildFloor.lua
+++ buildFloor.lua
@@ -1,27 +1,37 @@
 turtle.select(1)
+local direction = 1
 while true do
 
 	while not turtle.detect() do
 		turtle.forward()
-		
+
 		local slot = turtle.getSelectedSlot()
 		if turtle.getItemCount(slot) < 1 then
 			turtle.select(slot+1)
 		end
-		
+
 		if not turtle.detectDown() then
 			turtle.placeDown()
 		end
 	end
   
 	local slot = turtle.getSelectedSlot()
-  if turtle.getItemCount(slot) < 1 then
-    turtle.select(slot+1)
-  end
+	if turtle.getItemCount(slot) < 1 then
+		turtle.select(slot+1)
+	end
 	
-	turtle.turnLeft()
-	turtle.forward()
-	turtle.turnLeft()
-	turtle.placeDown()
+	if direction == 1 then
+		turtle.turnLeft()
+		turtle.forward()
+		turtle.turnLeft()
+		turtle.placeDown()
+		direction = 2
+	else
+		turtle.turnLeft()
+		turtle.forward()
+		turtle.turnLeft()
+		turtle.placeDown()
+		direction =1
+	end
 
 end
